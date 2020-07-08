d32fe9e4f955719798f67d71678a5703fe2e599d
SanFili <filimonova.alexandra@yandex.ru>
2020-07-08 18:14:07 +0300

    реализовано временное решение авторизации

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const path = require('path');
 
@@ -12,12 +13,23 @@ const cardsRouter = require('./routes/cards');
 mongoose.connect('mongodb://localhost:27017/mestodb', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  req.user = {
+      _id: '5f05e1a911016c19e6a2c0fd'
+  };
+
+  next();
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', usersRouter);
-app.use('/', cardsRouter);
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
 app.use((req, res) => {
   if (!res.headersSent) {
     res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,8 @@
 const usersRouter = require('express').Router();
-const userId = require('express').Router();
-const fs = require('fs').promises;
-const path = require('path');
 const { getUsers, getUserById, createUser } = require('../controllers/users');
-
-const users = (cbOk, cbErr) => {
-  const filepath = path.join(__dirname, '../data/users.json');
-  fs.readFile(filepath, { encoding: 'utf8' })
-    .then((data) => {
-      cbOk(JSON.parse(data));
-    })
-    .catch((err) => {
-      cbErr(err.message);
-    });
-};
 
 usersRouter.get('/', getUsers);
 usersRouter.get('/:id', getUserById);
 usersRouter.post('/', createUser);
 
-module.exports = { usersRouter };
+module.exports = usersRouter;
