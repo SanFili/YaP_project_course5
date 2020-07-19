ce62e7b440e5a8dcf140d0ccdbcc60713b68d0a7
SanFili <filimonova.alexandra@yandex.ru>
2020-07-19 16:12:25 +0300

    Исправлен auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,18 +2,17 @@ const jwt = require('jsonwebtoken');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  if (req.headers.cookie) {
+    const cookieToken = req.headers.cookie.replace('jwt=', '');
+    let payload;
+    try {
+      payload = jwt.verify(cookieToken, 'some-key');
+    } catch (err) {
+      return res.status(401).send({ message: 'Необходима авторизация' });
+    }
+    req.user = payload;
+  } else {
     return res.status(401).send({ message: 'Необходима авторизация' });
   }
-  const token = authorization.replace('Bearer ', '');
-
-  let payload;
-  try {
-    payload = jwt.verify(token, 'some-key');
-  } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
-  }
-  req.user = payload;
   next();
 };
