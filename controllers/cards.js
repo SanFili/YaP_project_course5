5e13e7a91cdf51341ab4bd66eb6b4de851490d8d
SanFili <filimonova.alexandra@yandex.ru>
2020-07-19 19:26:31 +0300

    больше у пользователей нет возможности удалять чужие карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -22,13 +22,20 @@ module.exports.createCard = (req, res) => {
 };
 
 module.exports.deleteCard = (req, res) => {
-  Card.findOneAndDelete(req.params.id).orFail()
-    .then((card) => res.status(200).send({ data: card }))
-    .catch((err) => {
-      if (err.name === 'DocumentNotFoundError') {
-        res.status(404).send({ message: 'Карточка не найдена' });
+  Card.findById(req.params.id)
+    .orFail(() => res.status(404).send({ message: 'Карточка не найдена' }))
+    .then((card) => {
+      if (card.owner.toString() === req.user._id) {
+        Card.findByIdAndRemove(req.params.id)
+          .then((card) => {
+            if (card !== null) {
+              res.send({ data: card });
+            }
+          })
+          .catch((err) => res.status(500).send({ message: err.message }))
       } else {
-        res.status(500).send({ message: err.message });
+        res.status(400).send({ message: 'К сожалению, Вы не можете удалить данную карточку'});
       }
-    });
+    })
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
