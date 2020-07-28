d375d1a0161aab0b9f7acb0d4655825ecfbd82c3
SanFili <filimonova.alexandra@yandex.ru>
2020-07-28 14:01:53 +0300

    Добавлена валидация для deleteCard

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,16 @@
 const cardsRouter = require('express').Router();
 
 const { getCards, createCard, deleteCard } = require('../controllers/cards');
+const { celebrate, Joi } = require('celebrate');
 
 cardsRouter.get('/', getCards);
 cardsRouter.post('/', createCard);
-cardsRouter.delete('/:id', deleteCard);
+cardsRouter.delete('/:id',
+  celebrate({
+    params: Joi.object().keys({
+      id: Joi.string().alphanum().length(24),
+    }),
+  }),
+  deleteCard);
 
 module.exports = cardsRouter;
