1cd21b8c3ce0ea8efd4508d0d413b90823e0502d
SanFili <filimonova.alexandra@yandex.ru>
2020-07-28 13:59:10 +0300

    Добавлена валидация для getUserById

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,14 @@
 const usersRouter = require('express').Router();
 const { getUsers, getUserById } = require('../controllers/users');
+const { celebrate, Joi } = require('celebrate');
 
 usersRouter.get('/', getUsers);
-usersRouter.get('/:id', getUserById);
+usersRouter.get('/:id',
+  celebrate({
+    params: Joi.object().keys({
+      id: Joi.string().alphanum().length(24),
+    }),
+  }),
+  getUserById);
 
 module.exports = usersRouter;
