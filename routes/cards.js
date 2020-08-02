235290a9a2975286997f7cf29e962d6170044799
SanFili <filimonova.alexandra@yandex.ru>
2020-08-02 17:28:19 +0300

    Добавлена валидация для метода создания карточки

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,17 @@
 const cardsRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { getCards, createCard, deleteCard } = require('../controllers/cards');
+const urlValid = require('../regexp/urlValidation');
 
 cardsRouter.get('/', getCards);
-cardsRouter.post('/', createCard);
+cardsRouter.post('/',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string().required().regex(urlValid),
+    })
+  }),
+  createCard);
 cardsRouter.delete('/:id',
   celebrate({
     params: Joi.object().keys({
